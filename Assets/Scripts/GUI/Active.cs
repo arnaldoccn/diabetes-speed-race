627becc1c9c144d87ed97ceb5519be1b872a587d
Arnaldo <arnaldoccn@gmail.com>
2014-08-24 16:03:01 -0300

    Positioned new track and respective return points and accelerated points;

---
+++ Assets/Scripts/GUI/Active.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Active : MonoBehaviour
+{
+	void Start () {
+
+        this.gameObject.SetActive(false);
+        this.gameObject.SetActive(true);
+	}
+
+}
